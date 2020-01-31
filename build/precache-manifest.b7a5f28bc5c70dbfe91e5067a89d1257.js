e21cbb06d6ef17b93a564186154db31229a45dfc
Alexanderqwer <llkkfi27456@gmail.com>
2020-01-31 17:38:49 +0200

    predeploy

---
+++ build/precache-manifest.b7a5f28bc5c70dbfe91e5067a89d1257.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5158cd28642e83137d67e75c4f7ff13",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "f24c66cf8f48e1a5e7e6",
+    "url": "/goit-react-hw-01-components/static/css/main.5744bcba.chunk.css"
+  },
+  {
+    "revision": "a6e6b035d664728aafed",
+    "url": "/goit-react-hw-01-components/static/js/2.9872b7bd.chunk.js"
+  },
+  {
+    "revision": "f24c66cf8f48e1a5e7e6",
+    "url": "/goit-react-hw-01-components/static/js/main.888af9b4.chunk.js"
+  },
+  {
+    "revision": "056a9cef59e001797fc0",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.2e65a945.js"
+  }
+]);
