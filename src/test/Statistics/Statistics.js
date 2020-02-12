e6f654ec1e303a5e24a48359eb7e78184f26f29d
Alexanderqwer <llkkfi27456@gmail.com>
2020-02-12 15:39:49 +0200

    hw

--- src/test/Statistics/Statistics.js
+++ src/test/Statistics/Statistics.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Statistics.module.css';
+import Styles from './Statistics.module.css';
 
 const Statistics = ({ title, statistics }) => (
-  <section className={styles.statisticsWrapper}>
+  <section className={Styles.statisticsWrapper}>
     {title && <h2 className="title">{title}</h2>}
 
-    <ul className={styles.list}>
+    <ul className={Styles.list}>
       {statistics.map(statistic => (
-        <li className={styles.item} key={statistic.id}>
+        <li className={Styles.item} key={statistic.id}>
           <span className="label">{statistic.label}</span>
-          <span className="percentage">{statistic.percentage}</span>
+          <span className="percentage">{statistic.percentage}%</span>
         </li>
       ))}
     </ul>
